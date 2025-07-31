6dedbc0119abb215a3a2b6a1db1175c51dfae53c
Donggeon Lee <dg981001@naver.com>
2025-07-31 18:01:45 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="crystal_video_maker",
     version="0.0.1",
-    python_requires='>=3.10',
+    python_requires='>=3.12',
     packages=find_packages(),
     install_requires=[
         "numpy",
         "plotly",
         "pymatgen",
-        "pandas",
+        "moviepy",
+        "imageio",
+        "pillow",
+        "ase"
     ],
     entry_points={
         "console_scripts": [
