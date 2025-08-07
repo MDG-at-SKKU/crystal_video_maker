4d095f28efa13ebaf4c732b27d06bc38226194ba
Donggeon Lee <dg981001@naver.com>
2025-08-07 17:04:03 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="crystal_video_maker",
     version="0.0.1",
-    python_requires='>=3.12',
+    python_requires=">=3.12",
     packages=find_packages(),
     install_requires=[
         "numpy",
